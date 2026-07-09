9881601325f724a0f00b1bf43f134d7f34fdef55
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2026-07-09 12:01:48 +0200

    fix: torch inference (#185)
    Co-authored-by: alfieroddan <51797647+alfieroddan@users.noreply.github.com>

--- src/physicalai/inference/preprocessors/__init__.py
+++ src/physicalai/inference/preprocessors/__init__.py
@@ -16,6 +16,7 @@ from physicalai.inference.preprocessors.pi05 import Pi05Preprocessor
 from physicalai.inference.preprocessors.resize import ResizeMode, ResizePreprocessor
 from physicalai.inference.preprocessors.smolvla import ResizeSmolVLA
 from physicalai.inference.preprocessors.stats_normalizer import StatsNormalizer
+from physicalai.inference.preprocessors.to_tensor import ToFloatTensorPreprocessor
 
 __all__ = [
     "HFTokenizer",
@@ -28,4 +29,5 @@ __all__ = [
     "ResizePreprocessor",
     "ResizeSmolVLA",
     "StatsNormalizer",
+    "ToFloatTensorPreprocessor",
 ]
