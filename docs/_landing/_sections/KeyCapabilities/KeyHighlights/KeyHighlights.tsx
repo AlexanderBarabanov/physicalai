c8a70ec7e2e0a69f1b69a50a6de66628b40e6925
Colorado, Camilo <camilo.colorado@intel.com>
2026-08-06 14:35:10 +0200

    doc: high definition icons

--- docs/_landing/_sections/KeyCapabilities/KeyHighlights/KeyHighlights.tsx
+++ docs/_landing/_sections/KeyCapabilities/KeyHighlights/KeyHighlights.tsx
@@ -1,9 +1,10 @@
 import openVinoIr from "../../../img/openvinoIr.png";
 import styles from "./KeyHighlights.module.css";
+import { DottedCard } from "../../../_components/DottedCard/DottedCardDetails";
 
 export const KeyHighlights = () => {
   return (
-    <div>
+    <DottedCard>
       <img src={openVinoIr} alt="OpenVINO IR" />
 
       <h3 className={styles.title}>Key highlights</h3>
@@ -12,11 +13,11 @@ export const KeyHighlights = () => {
         <li>Backend-agnostic (OV is one of backends)</li>
         <li>Configurable via manifest file</li>
         <li>
-          Allows utilizing custom pre/post processing nodes (for instance,
-          tokenization with OV tokenizers)
+          Allows utilizing custom pre/post processing nodes (for instance, tokenization with OV
+          tokenizers)
         </li>
         <li>Customizable model execution flow</li>
       </ul>
-    </div>
+    </DottedCard>
   );
 };
